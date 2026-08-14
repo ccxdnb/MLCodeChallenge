0c24818cfbfc3a2bbaf37e02e457d9a5e78b794e
Joaquin Wilson <joaquin@tryaiaio.com>
2026-08-14 17:44:35 -0300

    Initial Commit

---
+++ MLCodeChallenge/MLCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLCodeChallengeApp.swift
+//  MLCodeChallenge
+//
+//  Created by Joaquin Wilson on 8/14/26.
+//
+
+import SwiftUI
+
+@main
+struct MLCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
